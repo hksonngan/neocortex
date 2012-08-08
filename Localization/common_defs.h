ba4bf76f4c5260b14761d3702ccb01486c7ebb7c
alexkochenkov <alexkochenkov@a96e3a96-2080-f4de-a405-955615db7276>
2012-08-08 17:34:32 +0000

    Modified Base Math Library, Base Render Library and GLee (added static libraries) Added Post-build events to Neocortex

--- Localization/common_defs.h
+++ Localization/common_defs.h
@@ -5,11 +5,11 @@
 #include <math.h>
 #include <float.h>
 #include <typeinfo>
-#include "GLee/GLee.h"
+#include "GLee.h"
 #include <GL/gl.h>
-#include <Base Render Library\Keyboard.h>
-#include <Base Render Library\Mouse.h>
-#include <Base Render Library\FrameBuffer.h>
+#include <Keyboard.h>
+#include <Mouse.h>
+#include <FrameBuffer.h>
 
 #pragma comment(lib,"opengl32.lib")
 #pragma comment(lib,"glut32.lib") 
